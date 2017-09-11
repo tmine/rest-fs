bae31a643c6e638c66f95d621529c02c087af457
Adrian Bertschi <adrian.bertschi@gmail.com>
2017-09-11 11:53:17 +0000

    Refactoring

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-var path = require("path");
-var fs = require("fs");
 var rootDirectory = process.argv[2] || __dirname;    // TODO: use configuration parameter
 require("http").createServer(function(req, res) {
-    var relativePath = path.relative(rootDirectory, "." + req.url);
+    var relativePath = require("path").relative(rootDirectory, "." + req.url);
     var body = [];
     req.on('data', function(chunk){
         body.push(chunk);
@@ -54,42 +52,7 @@ require("http").createServer(function(req, res) {
 }).listen(process.env.PORT, process.env.IP);
 
 var implementation = (function Filesystem(){
-    function get(relativePath, success, error){
-        if(fs.existsSync(relativePath) && fs.statSync(relativePath).isFile()){
-            success(fs.createReadStream(relativePath));
-        } else {
-            error();
-        }
-    }
-    
-    function create(relativePath, body, success, error){
-        if(fs.existsSync(relativePath)){
-            error();
-        } else {
-            createDirectoryStructure(relativePath);
-            fs.writeFile(relativePath, body, success);
-        }
-    }
-    
-    function update(relativePath, body, success, error){
-        if(fs.existsSync(relativePath)){
-            createDirectoryStructure(relativePath);
-            fs.writeFile(relativePath, body, success);
-        } else {
-            error();
-        }
-    }
-    
-    function remove(relativePath, success, error){
-        if(fs.existsSync(relativePath) && fs.statSync(relativePath).isFile()){
-            fs.unlink(relativePath, function(){
-                // TODO: cleanup empty directories
-                success();
-            });
-        } else {
-            error();
-        }
-    }
+    var fs = require("fs");
     
     function createDirectoryStructure(dir){
         dir.split('/').reduce(function(prev, curr, i) {
@@ -101,9 +64,38 @@ var implementation = (function Filesystem(){
     }
     
     return {
-        get: get,
-        create: create,
-        update: update,
-        remove: remove
+        get: function(relativePath, success, error){
+            if(fs.existsSync(relativePath) && fs.statSync(relativePath).isFile()){
+                success(fs.createReadStream(relativePath));
+            } else {
+                error();
+            }
+        },
+        create: function(relativePath, body, success, error){
+            if(fs.existsSync(relativePath)){
+                error();
+            } else {
+                createDirectoryStructure(relativePath);
+                fs.writeFile(relativePath, body, success);
+            }
+        }, 
+        update: function(relativePath, body, success, error){
+            if(fs.existsSync(relativePath)){
+                createDirectoryStructure(relativePath);
+                fs.writeFile(relativePath, body, success);
+            } else {
+                error();
+            }
+        },
+        remove: function(relativePath, success, error){
+            if(fs.existsSync(relativePath) && fs.statSync(relativePath).isFile()){
+                fs.unlink(relativePath, function(){
+                    // TODO: cleanup empty directories
+                    success();
+                });
+            } else {
+                error();
+            }
+        }
     };
 })();
